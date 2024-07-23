e819652e554542ad4effbf11e9350c9997d3a124
kevinchauhan <rwnkevinchauhan@gmail.com>
2024-07-23 17:38:31 +0530

    add category controller

---
+++ src/category/category-controller.ts
@@ -0,0 +1,33 @@
+import { NextFunction, Request, Response } from "express";
+import { validationResult } from "express-validator";
+import createHttpError from "http-errors";
+import { Category } from "./category-types";
+import { CategoryService } from "./category-service";
+import { Logger } from "winston";
+
+export class CategoryController {
+    constructor(
+        private categoryService: CategoryService,
+        private logger: Logger,
+    ) {}
+
+    async create(req: Request, res: Response, next: NextFunction) {
+        const result = validationResult(req);
+
+        if (!result.isEmpty()) {
+            return next(createHttpError(400, result.array()[0].msg as string));
+        }
+
+        const { name, priceConfiguration, attributes } = req.body as Category;
+
+        const category = await this.categoryService.create({
+            name,
+            priceConfiguration,
+            attributes,
+        });
+
+        this.logger.info("New Category created", { id: category._id });
+
+        res.json({});
+    }
+}
